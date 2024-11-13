283555651cb47a51791cb50f8da1e8165586844a
effilin <effienummer@gmail.com>
2024-11-13 12:10:50 -0500

    creating welcome message with animation

--- my-homepage/src/app/app.component.ts
+++ my-homepage/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink,RouterLinkActive } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
